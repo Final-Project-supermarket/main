795e1c149715caef19dca03a57f6bc1d830698a2
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2022-03-20 15:48:30 +0200

    Fixed Connection to our DB

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
 const express = require('express');
-const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose")
 const app = express();
 const port = 3000;
 
-const mongoString = "mongodb+srv://nadav:nadav123@cluster0.gfmrz.mongodb.net/Project?retryWrites=true&w=majority"
+const mongoString = "mongodb+srv://nadav:nadav123@cluster0.gfmrz.mongodb.net/Supermarket?retryWrites=true&w=majority"
 mongoose.connect(mongoString, {useNewUrlParser: true})
 mongoose.connection.on("error", function(error) {
   console.log(error)
@@ -13,6 +11,15 @@ mongoose.connection.on("error", function(error) {
 mongoose.connection.on("open", function() {
   console.log("Connected to MongoDB database.")
 })
+
+app.get('/items',(req,res) =>{
+    res.status(200).json({
+        status:'Success',
+        data: {
+            items
+        }
+    });
+});
 
 app.listen(port,()=> {
     console.log(`App Running on port ${port}`);
