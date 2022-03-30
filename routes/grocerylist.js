de9171564da8b36b186dec928ff84b324bf0b459
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2022-03-30 17:41:30 +0300

    Algo Part Ready to be Implemented

--- routes/grocerylist.js
+++ routes/grocerylist.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getgroceryList, getgroceryLists, creategroceryList, updategroceryList, deletegroceryList } = require('../controllers/grocerylist');
+const { getbestList,getgroceryList, getgroceryLists, creategroceryList, updategroceryList, deletegroceryList } = require('../controllers/grocerylist');
 const router = express.Router();
 
 router.route('/').get(getgroceryLists).post(creategroceryList);
 router.route('/:id').get(getgroceryList).put(updategroceryList).delete(deletegroceryList);
+router.get('/getbestList/:id',getbestList)
 module.exports = router;
